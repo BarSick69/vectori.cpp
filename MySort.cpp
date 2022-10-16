b7e0f8ef53c1f0f971e98093cb5f28b6a343dc44
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2022-10-16 18:51:42 +0300

    Update MySort.cpp

--- MySort.cpp
+++ MySort.cpp
@@ -1,1 +1,41 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int recquicksort(vector<int> &v,int upperbound,int lowerbound){
+    bool change=false;
+    int temp =0;
+    if(upperbound<=lowerbound){
+        return 0;
+    }
+    for(int i = lowerbound;i<upperbound;i++){
+        if(v[upperbound]<v[i]){
+            temp=v[upperbound];
+            v[upperbound] =v[i];
+            v[i] = temp;
+            change = true;
+            break;
+        }
+    }
+    if(change){
+        recquicksort(v,upperbound,lowerbound);
+    }
+    else if(!change){
+        upperbound-=1;
+        recquicksort(v,upperbound,lowerbound);
+    }
+    return 0;
+}
+int main() {
+    // Write C++ code here
+    cout << "Hello world!";
+    cout <<endl;
+    vector<int> v={3,2,1,62,85,123,-23};
+    recquicksort(v,v.size()-1,0);
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    
+    return 0;
+}
